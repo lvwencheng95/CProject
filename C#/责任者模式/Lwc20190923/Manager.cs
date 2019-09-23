23aeae21c7d814d05edf3f2c38ea264861dabf46
lvwencheng <525955465@qq.com>
2019-09-23 16:30:37 +0800

    责任者模式

---
+++ C#/责任者模式/Lwc20190923/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lwc20190923
+{
+    /// <summary>
+    /// 经理
+    /// </summary>
+    public class Manager : Approver
+    {
+        //初始化基类
+        public Manager(string name)
+            : base(name)
+        { }
+
+        public override void ProcessRequest(PurchaseRequest request)
+        {
+            if (request.Amount < 10000)
+            {
+                Console.WriteLine("{0}-{1} approved the request of purshing {2}", this.GetType().Name, Name, request.ProductName);
+            }
+            else if (NextApprover != null)
+            {
+                NextApprover.ProcessRequest(request);
+            }
+        }
+    }
+}
---
+++ C#/责任者模式/Lwc20190923/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lwc20190923
+{
+    class Program
+    {
+        /*
+         *责任链模式类似击鼓传花，击鼓相当于设置条件，哪朵花适合。其实可以多层if-else if判断，使用责任链模式
+         *有点类似递归，每次再调用相同的方法，只是传入的参数不一样。
+         *
+         *代码特点：
+         *(1)定义抽象类，具体的经理、总经理类继承抽象类。
+         *(2)设置责任链，比如经理不能决定，找总经理
+         */
+        static void Main(string[] args)
+        {
+            //当采购产品金额大于10万
+            PurchaseRequest requestServer = new PurchaseRequest(120000,"服务器");
+            PurchaseRequest requestComputer = new PurchaseRequest(2000,"电脑");
+
+
+            Approver manager = new Manager("张山");
+            Approver present = new President("李斯格");
+
+            //设置责任链
+            manager.NextApprover = present;//经理不能决定，找总经理
+
+            //处理采购任务
+            //采购服务器
+            manager.ProcessRequest(requestServer);
+
+            //采购电脑
+            manager.ProcessRequest(requestComputer);
+
+            Console.ReadLine();
+        }
+    }
+}
